71b14fec8e523469d322a6ca84150514db0b03b4
muxianliangqin <34302273+muxianliangqin@users.noreply.github.com>
2019-07-16 15:44:33 +0800

    添加抓取正文

--- crawler-assistant/js/content-script.js
+++ crawler-assistant/js/content-script.js
@@ -18,18 +18,31 @@ document.addEventListener('DOMContentLoaded', function()
 
     console.log('监听鼠标点击事件');
 	document.onmousedown = function(e){
-		if (e.button==2 && !show_context_menu && $('.crawler-result-show').length == 0){
+		if (e.button=== 2 && !show_context_menu && $('.crawler-result-show').length == 0){
 			// let show = $('.crawler-result-show');
 			let xpath = getXpath(e);
-            if (action == 'title') {
+            if (action === 'title') {
                 let res = getTitle(e);
-                res.xpath = xpath;
+                res.xpathTitle = xpath;
                 resultShow(res);
-            } else if (action == 'text') {
+            } else if (action === 'text') {
                 let text = getText(e);
-                chrome.storage.local.get({titleInfo: {}}, function (value) {
-                    value.text = text;
-                    resultShow(value);
+                let domain = e.currentTarget.domain;
+                chrome.storage.local.get({'result': {}}, function (value) {
+                    let res = value.result;
+                    if (res.origin !== domain) {
+                        res = {
+                            origin: '',
+                            baseURI: '',
+                            title: '',
+                            charset: '',
+                            validResults: [],
+                            invalidResults: []
+                        };
+                    }
+                    res.text = text;
+                    res.xpathText = xpath;
+                    resultShow(res);
                 });
 
             }
@@ -47,63 +60,62 @@ document.addEventListener('DOMContentLoaded', function()
 
     function getXpath(e){
         // 去除window、document、html等,从body开始
-        let paths = e.path
+        let paths = e.path;
         paths.reverse();
         paths = paths.slice(3);
         // 寻找最后一个具有id属性的元素
         paths = paths.reverse();
-        var index = paths.length;
-        for(var i=0;i<paths.length;i++){
-            var v = paths[i];
+        let index = paths.length;
+        for(let i=0;i<paths.length;i++){
+            let v = paths[i];
             if (v.id) {
                 index = i + 1;
                 break;
             }
         }
-        paths = paths.slice(0,index)
+        paths = paths.slice(0,index);
         //获取xpath值
-        paths = paths.reverse()
-        var xp = ['']
-        var children = null
-        for(var i=0;i<paths.length;i++){
-            var path = paths[i]
+        paths = paths.reverse();
+        let xp = [''];
+        let children = null;
+        for(let i=0;i<paths.length;i++){
+            let path = paths[i];
             // 如果元素有id，这里通常是第一个
             if (path.id) {
-                xp.push('/*[@id="'+path.id+'"]')
-                children = path.children
+                xp.push('/*[@id="'+path.id+'"]');
+                children = path.children;
                 continue
             }
             // children == null时，这里通常是html/body
             if (children == null) {
-                xp.push('/' + path.localName)
-                children = path.children
+                xp.push('/' + path.localName);
+                children = path.children;
                 continue
             }
             // 只保留元素名相同的
-            var x_children = []
-            for(var j=0;j<children.length;j++){
-                if (children[j].localName == path.localName){
+            let x_children = [];
+            for(let j=0;j<children.length;j++){
+                if (children[j].localName === path.localName){
                     x_children.push(children[j])
                 }
             }
             // 获得xpath序号
-            if (x_children.length == 1) {
+            if (x_children.length === 1) {
                 xp.push(path.localName)
             } else {
-                var x_idx = 0
-                for(var j=0;j<x_children.length;j++){
-                    if (x_children[j] == path){
-                        xp.push(path.localName+'['+(x_idx+1)+']')
+                let x_idx = 0;
+                for(let j=0;j<x_children.length;j++){
+                    if (x_children[j] === path){
+                        xp.push(path.localName+'['+(x_idx+1)+']');
                         break
                     } else {
-                        x_idx = x_idx + 1
+                        x_idx = x_idx + 1;
                     }
                 }
             }
             children = path.children
         }
-        var xpath = xp.join('/')
-        return xpath
+        return xp.join('/');
     }
 
 	function getTitle(e){
@@ -115,21 +127,19 @@ document.addEventListener('DOMContentLoaded', function()
             baseURI: currentTarget.baseURI,
             title: currentTarget.title,
             charset: currentTarget.charset,
-            clientWidth: currentTarget.documentElement.clientWidth,
-            clientHeight: currentTarget.documentElement.clientHeight,
             validResults: [],
             invalidResults: []
-        }
+        };
         let invalidTitle = /^(首页|下一页|上一页|确定|末页|尾页|更多(\s+>>)?|\d+)$/;
         let invalidHref = /^(\.\/|\.\.\/)((\.\.\/)+)?$/;
-		for (var i=0;i<as.length;i++){
-			var a = as[i];
-			var title = a.title;
+		for (let i=0;i<as.length;i++){
+			let a = as[i];
+			let title = a.title;
 			if (!title) {
                 title = a.innerText
             }
             let href = a.attributes['href'].nodeValue;
-			if (invalidTitle.test(title) | invalidHref.test(href)) {
+			if (invalidTitle.test(title) || invalidHref.test(href)) {
                 res.invalidResults.push({
                     'title':title
                 })
@@ -157,8 +167,8 @@ document.addEventListener('DOMContentLoaded', function()
 			    </h3>
 			    <table style="width: 100%">
 			        <colgroup>
-                        <col width="30%">
-                        <col width="70%">
+                        <col width="20%">
+                        <col width="80%">
                     </colgroup>
 			        <tbody>
 			            <tr>
@@ -178,8 +188,12 @@ document.addEventListener('DOMContentLoaded', function()
 			                <td id="self-web-info-charset"></td>
                         </tr>
                         <tr>
-			                <td>元素路径</td>
-			                <td id="self-web-info-xpath"></td>
+			                <td>标题路径</td>
+			                <td id="self-web-info-xpath-title"></td>
+                        </tr>
+                        <tr>
+			                <td>正文路径</td>
+			                <td id="self-web-info-xpath-text"></td>
                         </tr>
                     </tbody>
                 </table>
@@ -206,23 +220,27 @@ document.addEventListener('DOMContentLoaded', function()
         div.find('#self-web-info-title').html(res.title);
         div.find('#self-web-info-baseURI').html(res.baseURI);
         div.find('#self-web-info-charset').html(res.charset);
-        div.find('#self-web-info-xpath').html(res.xpath);
+        div.find('#self-web-info-xpath-title').html(res.xpathTitle);
+        div.find('#self-web-info-xpath-text').html(res.xpathText);
+        const height = document.documentElement.clientHeight;
+        const width = document.documentElement.clientWidth;
         div.find('.self-res-div').css({
-            'max-height': res.clientHeight - 350 + 'px',
-            'max-width': res.clientWidth * 0.5 + 'px',
+            'max-height': height - 350 + 'px',
+            'max-width': width * 0.5 + 'px',
         });
-        showTip(action)
+        const tip = div.find('#self-span-tip');
+        showTip(tip, action);
         div.find('#self-button-switch').click(function () {
-        	action = action == 'title'?'text':'title';
+        	action = action === 'title'?'text':'title';
             saveLocalStorage({'action':action});
-            showTip(action)
+            showTip(tip, action);
         });
-        var tbody = div.find('#self-result-table tbody');
-        tbody.append($('<tr><td colspan="2">有效结果</td></tr>'));
-        if (action == 'title') {
+        let tbody = div.find('#self-result-table tbody');
+        if (action === 'title') {
+            tbody.append($('<tr><td colspan="2">标题有效结果</td></tr>'));
             let validResults = res.validResults;
-            for (var i=0;i<validResults.length;i++){
-                var a = validResults[i];
+            for (let i=0;i<validResults.length;i++){
+                let a = validResults[i];
                 tbody.append(
                     $('<tr>' +
                         '<td style="text-align: right;">'+(i+1)+'</td>' +
@@ -230,10 +248,10 @@ document.addEventListener('DOMContentLoaded', function()
                         '</tr>')
                 );
             }
-            tbody.append($('<tr><td colspan="2">无效结果</td></tr>'))
+            tbody.append($('<tr><td colspan="2">标题无效结果</td></tr>'));
             let invalidResults = res.invalidResults;
-            for (var i=0;i<invalidResults.length;i++){
-                var a = invalidResults[i];
+            for (let i=0;i<invalidResults.length;i++){
+                let a = invalidResults[i];
                 tbody.append(
                     $('<tr>' +
                         '<td style="text-align: right;">'+(i+1)+'</td>' +
@@ -241,25 +259,50 @@ document.addEventListener('DOMContentLoaded', function()
                         '</tr>')
                 );
             }
-		} else if (action == 'text') {
-            tbody.append($('<tr><td colspan="2">' + res.text + '</td></tr>'));
+        } else if (action === 'text') {
+            let validResults = res.validResults;
+            let trs = [];
+            trs.push($('<tr><td colspan="2">标题结果(共'+validResults.length+'条)</td></tr>'));
+            for (let i=0;i<validResults.length;i++) {
+                let a = validResults[i];
+                trs.push(
+                    $('<tr>' +
+                        '<td style="text-align: right;">' + (i + 1) + '</td>' +
+                        '<td><a href=' + a.href + ' target="_blank">' + a.title + '</a></td>' +
+                        '</tr>')
+                );
+            }
+            trs.splice(3,trs.length-5,$('<tr><td colspan="2">......</td></tr>'));
+            tbody.html(trs);
+
+            let text = res.text;
+            tbody.append($('<tr><td colspan="2">正文结果(约' + text.length + '字)</td></tr>'));
+            if (text.length > 100) {
+                text = text.substr(0,50) + '<br>......<br>' + text.substr(-50);
+            }
+            tbody.append($('<tr><td colspan="2" style="width: 20em">' + text + '</td></tr>'));
 		}
 		let button_submit = $('<button id="self-button-submit" style="margin:0 10px;">确定选择</button>');
         button_submit.click(function(){
-            confirm_titles(res);
+            save_result(res);
         });
-        let button_select_text = $('<button id="self-button-select-text" style="margin:0 10px;display: none">选择正文</button>');
+        let button_select_text = $('<button id="self-button-select-text" style="margin:0 10px;">选择正文</button>');
         button_select_text.click(function(){
             tbody.find('a:first')[0].click();
-            saveLocalStorage({action:'text'});
-            delete res.validResults;
-            delete res.invalidResults;
-            saveLocalStorage({titleInfo:res});
-            remove_titles_show();
+            saveLocalStorage({'action':'text'});
+            saveLocalStorage({'result':res});
+            remove_result_show();
         });
+        if (action === 'title') {
+            button_submit.hide();
+            button_select_text.show();
+        } else if (action === 'text') {
+            button_submit.show();
+            button_select_text.hide();
+        }
         let button_reselect = $('<button id="self-button-reselect" style="margin:0 10px;">重新选择</button>');
         button_reselect.click(function(){
-            remove_titles_show();
+            remove_result_show();
         });
         let footer = div.find('.self-footer');
         footer.append(button_submit);
@@ -268,16 +311,12 @@ document.addEventListener('DOMContentLoaded', function()
 		$('body').append(div);
 	}
 
-	function showTip(action) {
-        $('#self-span-tip').html(action == 'title'?'抓取标题':'抓取正文');
+	function showTip(ele, action) {
+        ele.html(action === 'title'?'抓取标题':'抓取正文');
     }
 
-	function confirm_titles(res){
-        alert('添加成功');
-        $('#self-button-submit').hide();
-        $('#self-button-reselect').show();
-        $('#self-button-select-text').show();
-        /*let config = {
+	function save_result(res){
+        let config = {
             userInfo:null
         };
         chrome.storage.sync.get(config,function (value) {
@@ -285,10 +324,9 @@ document.addEventListener('DOMContentLoaded', function()
             if (userInfo) {
                 res.userId = userInfo.id;
                 $.ajax({
-                    url:'http://47.106.140.189/crawler/plugin/save',
-                    // url:'http://localhost:7020/plugin/save',
+                    // url:'http://47.106.140.189/crawler/plugin/save',
+                    url:'http://localhost:7020/plugin/save',
                     type: 'post',
-                    // traditional: true,
                     data: {
                         crawlerResult: JSON.stringify(res)
 					},
@@ -296,36 +334,36 @@ document.addEventListener('DOMContentLoaded', function()
 					dataType: 'json'
                 }).success(function(response){
                     alert('添加成功');
-                    remove_titles_show()
+                    remove_result_show();
                 }).fail(function (response) {
                     alert('添加失败')
                 })
             } else {
                 alert('操作失败，请前往【chrome设置->更多工具->扩展程序->详细信息->扩展程序选项】配置秘钥')
             }
-        })*/
+        })
 	}
 
-	function remove_titles_show () {
-		$('.crawler-result-show').remove()
-		show_context_menu = false
-		show_border = true
+	function remove_result_show () {
+		$('.crawler-result-show').remove();
+		show_context_menu = false;
+		show_border = true;
 	}
 
 	function getText(e){
-		let text = e.srcElement.innerText;
-		return text;
+		return e.srcElement.innerText;
 	}
 
 	document.onmouseover = function(e){
 		if (show_border){
 			$(e.srcElement).addClass('myself-mouseover-selected')
 		}
-	}
+	};
+
 	document.onmouseout = function(e){
 		if (show_border){
 			$(e.srcElement).removeClass('myself-mouseover-selected')
 		}
-	}
+	};
 
 });
